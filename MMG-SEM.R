5300c15656ae1ed9197ccd8dcac7a2c9ef5de269
Andres Perez Alonso <A.F.PerezAlonso@tilburguniversity.edu>
2023-12-05 10:52:01 +0100

    Update MMG-SEM.R

--- MMG-SEM.R
+++ MMG-SEM.R
@@ -54,7 +54,7 @@
 #'
 #' PLEASE NOTE: This function requires 'lavaan' package to work. It also requires the function 'EStep.R'.
 #' ################################# ALL GLOBAL SAM CODE IS NOT FUNCTIONAL YET #########################################
-
+#' @export
 MMGSEM <- function(dat, step1model = NULL, step2model = NULL,
                    group, nclus, seed = NULL, userStart = NULL, s1out = NULL,
                    max_it = 10000L, nstarts = 20L, printing = FALSE,
@@ -1315,3 +1315,25 @@ MMGSEM <- function(dat, step1model = NULL, step2model = NULL,
     N_gs          = N_gs
   ))
 }
+
+EStep <- function(pi_ks, ngroup, nclus, loglik){
+  
+  max_g <-rep(0,ngroup)
+  z_gks <- matrix(NA,nrow = ngroup,ncol = nclus)
+  
+  for(g in 1:ngroup){
+    for(k in 1:nclus){
+      z_gks[g,k] <- log(pi_ks[k])+loglik[g,k]
+    }
+    max_g[g] <- max(z_gks[g,]) # prevent arithmetic underflow 
+    z_gks[g,] <- exp(z_gks[g,]-rep(max_g[g],nclus))
+  }
+  
+  # divide by the rowwise sum of the above calculated part 
+  z_gks <- diag(1/apply(z_gks,1,sum))%*%z_gks
+  # z_gks <- round(z_gks, digits = 16)
+  # z_gks <- diag(1/apply(z_gks,1,sum))%*%z_gks
+  
+  return(z_gks)
+}
+
